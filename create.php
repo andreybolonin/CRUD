593ff63bca7b28bfe1ed29b33aceb3fae3bb1a37
Andrey Bolonin <andreybolonin1989@gmail.com>
2017-04-30 17:45:37 +0300

    added CRUD

---
+++ create.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'model/model.php';
+
+if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
+
+    insert($_POST['name'], $_POST['description'], $_POST['created_at']);
+
+    header('Location: index.php');
+}
+
+require_once 'view/createTemplate.php';
--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 require_once 'model/model.php';
 
-if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
+$rows = findAll();
 
-    insert($_POST['name'], $_POST['description'], $_POST['created_at']);
-}
-
-require_once 'view/indexTemplate.php';
+require_once  'view/indexTemplate.php';
