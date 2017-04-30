9189882249a4e9bad365e9d1ad8e25fda42a8ea1
Andrey Bolonin <andreybolonin1989@gmail.com>
2017-04-30 15:24:22 +0300

    refactor createAction to mvc

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
-<form method="post" action="index.php">
-    <input type="text" placeholder="name" name="name">
-    <br>
-    <input type="text" placeholder="description" name="description">
-    <br>
-    <input type="text" placeholder="created_at" name="created_at">
-    <br>
-    <input type="submit">
-</form>
-
 <?php
+
+require_once 'model/model.php';
 
 if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
 
-    var_dump($_POST['name']);
-    $pdo_conn = new PDO("mysql:host=127.0.0.1;dbname=CRUD", 'dbuser', '123');
-
-    $sql = 'INSERT INTO article (id, name, description, created_at) VALUES (:id, :name, :description, :created_at)';
-    $statement = $pdo_conn->prepare($sql);
-    $statement->bindValue(':id', 1);
-    $statement->bindValue(':name', $_POST['name']);
-    $statement->bindValue(':description', $_POST['description']);
-    $statement->bindValue(':created_at', $_POST['created_at']);
-
-    var_dump($statement->execute());
-    var_dump($statement->errorInfo());
+    insert($_POST['name'], $_POST['description'], $_POST['created_at']);
 }
+
+require_once 'view/indexTemplate.php';
